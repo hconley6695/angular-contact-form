b5ec94048587ed4581513ead6bc58bcb19d041c0
Heather Conley <hconley6695@gmail.com>
2016-11-28 13:12:26 -0500

    adding errors to js and html

--- src/js/maincontroller.js
+++ src/js/maincontroller.js
@@ -1,16 +1,45 @@
 
-function MainController($scope) {
+function MainController($scope, $http) {
 	$scope.contacts = [];
+	$scope.errors = {};
+
+
+	$scope.validateName = function (name) {
+		if (name === '') {
+			$scope.errors.name = "You must supply a name."
+		};
+	};
+
+	// $scope.validateEmail = function (emailAddress) {
+	// 	if (emailAddress === '' || emailAddress.includes('@')) {
+	// 		$scope.errors.emailAddress = "You must supply an email address."
+	// 	}; 
+	// }
+
+
+
+	// $scope.validateWebsite = function (site) {
+	// 	if (site === '' || site.startsWith('http://') || site.startsWith('https://')) {
+	// 		$scope.errors.site = "You must supply a valid website."
+	// 	};
+	// }
+
+	// $scope.validateMsg = function (msg) {
+	// 	if (msg === '') {
+	// 		$scope.errors.msg = "You must fill out a comment."
+	// 	};
+	// }
+	
+
+	// 'https://class-server.herokuapp.com/collections/hconley-contactform'
 
 	$scope.addContact = function (person) {
-		let newContact = { fullname: fullname, email: email, website: website, comments: comments};
-		$scope.contacts.push();
-		console.log($scope.contacts);
+		
 	}
-	
+
 
 	console.log("Am I doing this correctly?");
 }
 
-MainController.$inject = ['$scope'];
+MainController.$inject = ['$scope', '$http'];
 export { MainController };
