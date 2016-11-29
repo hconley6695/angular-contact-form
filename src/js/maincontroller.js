2d92302f51390149e6e661c1f8501385546ee634
Heather Conley <hconley6695@gmail.com>
2016-11-29 09:49:50 -0500

    have templates on email for all of the inputs, added ng-model and ng-keyup on html, have uncommented the validation functions on maincontroller, some parts working

--- src/js/maincontroller.js
+++ src/js/maincontroller.js
@@ -7,39 +7,57 @@ function MainController($scope, $http) {
 	$scope.validateName = function (name) {
 		if (name === '') {
 			$scope.errors.name = "You must supply a name."
+
 		};
+		// HAD THIS LINE IN TO CLEAR TYPED IN DATA
+		// $scope.errors = '';		
 	};
 
-	// $scope.validateEmail = function (emailAddress) {
-	// 	if (emailAddress === '' || emailAddress.includes('@')) {
-	// 		$scope.errors.emailAddress = "You must supply an email address."
-	// 	}; 
-	// }
+	$scope.validateEmail = function (emailAddress) {
+		if (emailAddress === '' || emailAddress.includes('@')) {
+			$scope.errors.emailAddress = "You must supply an email address."
+		}; 
+	}
 
 
 
-	// $scope.validateWebsite = function (site) {
-	// 	if (site === '' || site.startsWith('http://') || site.startsWith('https://')) {
-	// 		$scope.errors.site = "You must supply a valid website."
-	// 	};
-	// }
+	$scope.validateWebsite = function (site) {
+		if (site === '' || site.startsWith('http://') || site.startsWith('https://')) {
+			$scope.errors.site = "You must supply a valid website."
+		};
+	}
 
-	// $scope.validateMsg = function (msg) {
-	// 	if (msg === '') {
-	// 		$scope.errors.msg = "You must fill out a comment."
-	// 	};
-	// }
+	$scope.validateMsg = function (msg) {
+		if (msg === '') {
+			$scope.errors.msg = "You must fill out a comment."
+		};
+	}
 	
 
 	// 'https://class-server.herokuapp.com/collections/hconley-contactform'
 
 	$scope.addContact = function (person) {
+		let newPerson = {
+			name: name, 
+			// email: email, 
+			// website: website, 
+			// comments: comments, 
+			id: $scope.contacts.length + 1
+		};
 		
+		console.log (newPerson);		
+		
+		$scope.contacts.push(newPerson);
+		console.log($scope.contacts);
 	}
 
-
+	console.log($scope.contacts);
 	console.log("Am I doing this correctly?");
 }
 
 MainController.$inject = ['$scope', '$http'];
 export { MainController };
+
+
+
+
