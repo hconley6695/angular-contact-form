c0fbfeb28cd336f005d8f577cf2a750b8bbdafb5
Heather Conley <hconley6695@gmail.com>
2016-11-30 12:40:38 -0500

    added server, communicating with server but not all information is showing up

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 // Javascript Entry Point
 import angular from 'angular';
 import { MainController } from './maincontroller';
+import { UserService } from './user-service';
 
 angular
 	.module('app', [])
-	.controller('MainController', MainController);
+	.controller('MainController', MainController)
+	.service('UserService', UserService);
--- src/js/maincontroller.js
+++ src/js/maincontroller.js
@@ -1,17 +1,71 @@
 
+import { SERVER } from './server';
+
 function MainController($scope, $http) {
 	$scope.contacts = [];
-	$scope.errors = {};
+	// $scope.errors = {};
 
 
-	$scope.validateName = function (name) {
-		if (name === '') {
-			$scope.errors.name = "You must supply a name."
+	function init() {
 
-		};
-		// HAD THIS LINE IN TO CLEAR TYPED IN DATA
-		// $scope.errors = '';		
+		console.log($scope.contacts);
+
+		$http.get(SERVER).then((resp) => {
+			console.log(resp.data);
+
+			$scope.contacts = resp.data;
+			$scope.contacts.splice(0, 4);
+			$scope.contacts.splice(2, 6);
+
+			console.log($scope.contacts);
+
+		});
+		
+
+
 	};
+
+	init();
+
+	$scope.addContact = function (person) {
+
+
+		$http.post(SERVER, person).then((resp) => {
+			//CREATED NEW OBJECT
+			console.log (resp);		
+
+			let person = {
+				name: resp.name, 
+				email: resp.email, 
+				website: resp.website, 
+				comments: resp.comments, 
+				id: $scope.contacts.length + 1
+			};
+
+			// $scope.contacts.push(person);
+
+		})
+		
+	
+		//ADDING NEW PERSON TO ARRAY NAMED CONTACTS
+
+		console.log($scope.contacts);
+		//CLEARING INPUT SO THAT PLACEHOLDERS SHOW UP AFTER PRESS SUBMIT BUTTON
+		$scope.person.name = " ";
+		$scope.person.email = " ";
+		$scope.person.website = " ";
+		$scope.person.comments = " ";
+	}
+
+
+	// $scope.validateName = function (name) {
+	// 	if (name === '') {
+	// 		$scope.errors.name = "You must supply a name."
+
+	// 	};
+	// 	// HAD THIS LINE IN TO CLEAR TYPED IN DATA
+	// 	// $scope.errors = '';		
+	// };
 
 	// $scope.validateEmail = function (email) {
 	// 	if  (email !== email.includes('@')) {
@@ -40,30 +94,6 @@ function MainController($scope, $http) {
 	// }
 	
 
-	// 'https://class-server.herokuapp.com/collections/hconley-contactform'
-
-	$scope.addContact = function (person) {
-		let newPerson = {
-			name: person.name, 
-			email: person.email, 
-			website: person.website, 
-			comments: person.comments, 
-			id: $scope.contacts.length + 1
-		};
-		
-		console.log (newPerson);		
-		
-		$scope.contacts.push(newPerson);
-		console.log($scope.contacts);
-
-		$scope.person.name = " ";
-		$scope.person.email = " ";
-		$scope.person.website = " ";
-		$scope.person.comments = " ";
-	}
-
-	// console.log($scope.contacts);
-	// console.log("Am I doing this correctly?");
 }
 
 MainController.$inject = ['$scope', '$http'];
