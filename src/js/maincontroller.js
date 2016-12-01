d6333f609e11bcafc097f050796b60a563c2cb4d
Heather Conley <hconley6695@gmail.com>
2016-12-01 14:42:39 -0500

    working on validations for name and comments.  only working after I delete the comment, not when I press submit button

--- src/js/maincontroller.js
+++ src/js/maincontroller.js
@@ -15,27 +15,31 @@ function MainController($scope, $http) {
 	init();
 
 	$scope.addContact = function (person) {
+		// if ($scope.validateEmail(person.email) && $scope.validateName(person.name) && $scope.validateWebsite(person.site) && $scope.validateMsg(person.msg)) {
+		if ($scope.validateName(person.name) && $scope.validateMsg(person.comments)) {	
+			$http.post(SERVER, person).then((resp) => {
+				//CREATED NEW OBJECT
+				console.log ('response', resp);		
+				let person = resp.data; 
 
-		$http.post(SERVER, person).then((resp) => {
-			//CREATED NEW OBJECT
-			console.log ('response', resp);		
-			let person = resp.data; 
-
-			console.log('person', person);
+				console.log('person', person);
 
 
-			//ADDING NEW PERSON TO ARRAY NAMED CONTACTS
-			$scope.contacts.push(person);
-			console.log($scope.contacts);
+				//ADDING NEW PERSON TO ARRAY NAMED CONTACTS
+				$scope.contacts.push(person);
+				console.log($scope.contacts);
 
-			//CLEARING INPUT SO THAT PLACEHOLDERS SHOW UP AFTER PRESS SUBMIT BUTTON
-			$scope.person.name = " ";
-			$scope.person.email = " ";
-			$scope.person.website = " ";
-			$scope.person.comments = " ";		
+				//CLEARING INPUT SO THAT PLACEHOLDERS SHOW UP AFTER PRESS SUBMIT BUTTON
+				$scope.person.name = " ";
+				$scope.person.email = " ";
+				$scope.person.website = " ";
+				$scope.person.comments = " ";		
 
-		});
-	}
+			});
+		} else {
+			console.log("You have a problem.");
+		}
+	};
 
 	$scope.deleteItem = function (person) {
 		console.log(person._id);
@@ -49,40 +53,46 @@ function MainController($scope, $http) {
 	}
 
 
-	// $scope.validateName = function (name) {
-	// 	if (name === '') {
-	// 		$scope.errors.name = "You must supply a name."
+	$scope.validateName = function (name) {
+		if (name === '') {
+			$scope.errors.name = "You must supply a name."
 
-	// 	};
-		// HAD THIS LINE IN TO CLEAR TYPED IN DATA
-		// $scope.errors = '';		
-	// };
+		};
+	};
 
 	// $scope.validateEmail = function (email) {
-	// 	if  (email !== email.includes('@')) {
+	// 	if  (!email.includes('@')) {
 	// 		$scope.errors.email = "You must include an @ in your email address." 
+	// 		return false;
 	// 	}
-	// 	else if (email === '' ) {
-	// 		$scope.errors.email = "You must supply an email address."
-	// 	} else {
-	// 		$scope.errors.email = "I don't know what I'm doing."
-	// 	}; 
+		
+	// 	if (email === '' ) {
+	// 		$scope.errors.email = " ";
+	// 	} 
+
+	// 	return true;
 	// }
 
 
 
 	// $scope.validateWebsite = function (site) {
-	// 	// || site.startsWith('http://') || site.startsWith('https://')
+	// 	if (!site.startsWith('http')) {
+	// 		$scope.errors.site = "You must supply a valid web address starting with http:// or https://"
+	// 		return false;
+	// 	}
+
 	// 	if (site === '' ) {
-	// 		$scope.errors.site = "You must supply a valid website."
+	// 		$scope.errors.site = " ";
 	// 	};
+
+	// 	return true;
 	// }
 
-	// $scope.validateMsg = function (msg) {
-	// 	if (msg === '') {
-	// 		$scope.errors.msg = "You must fill out a comment."
-	// 	};
-	// }
+	$scope.validateMsg = function (comments) {
+		if (comments === '') {
+			$scope.errors.comments = "You must fill out a comment."
+		};
+	}
 	
 
 }
